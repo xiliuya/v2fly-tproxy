f12f7815ae08248203e52cf2797d8fe629c9c20b
xiliuya <xiliuya@aliyun.com>
2026-07-24 11:17:53 +0800

    - add common_function.sh

---
+++ common_function.sh
@@ -0,0 +1,28 @@
+config_file="/data/adb/v2fly/v2ray.conf"
+
+load_conf_var() {
+	var=$1
+	default=$2
+
+	if [ -f "$config_file" ]; then
+		while IFS='=' read -r key value || [ -n "$key" ]; do
+			case "$key" in
+			'' | '#'* | *[!a-zA-Z0-9_]* | [0-9]* | *[[:space:]]*) continue ;;
+			esac
+			case "$value" in
+			*[[:space:]]* | *\`*) continue ;;
+			esac
+
+			if [ "$key" = "$var" ]; then
+				eval "${key}=\${value:-\$default}"
+				return
+			fi
+		done <"$config_file"
+	fi
+
+	eval "val=\$$var"
+	if [ -z "$val" ]; then
+		eval "${var}=\$default"
+	fi
+}
+
